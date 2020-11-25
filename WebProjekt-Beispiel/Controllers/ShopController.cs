06288b281cec16a94e2ea85696324f51c0941604
NoF4ll <neunermaxi@gmail.com>
2020-11-25 13:30:10 +0100

    First Upload

---
+++ WebProjekt-Beispiel/Controllers/ShopController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebProjekt_Beispiel.Models;
+     
+namespace WebProjekt_Beispiel.Controllers
+{    
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(CreateArticeList());
+        }
+
+        public IActionResult ReturnArticles()
+        {
+            return View();
+        }
+
+
+        private List<Article> CreateArticeList()
+        {
+            return new List<Article>()
+            {
+                new Article(1, "FNATIC Jersy", 60.99m, "Fnatic 2020 JERSY Custom Name", new DateTime(2020, 5, 20)),
+                new Article(2, "Fnatic DX Racer Gaming Stuhl", 290.99m, "Offizieller Gaming Stuhl der Fnatic Spieler", null),
+                new Article(3, "Streak 65 Esports Keyboard ", 119.90m, "Gaming Tastatur mit echten Cherry MX Switches", new DateTime(2019, 9, 1)),
+                new Article(4, "React Esports Performance Headset", 79.99m, "Esports Headset", new DateTime(2020, 5, 20))
+            };
+        }
+    }
+}
---
+++ WebProjekt-Beispiel/Models/Article.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProjekt_Beispiel.Models
+{
+    public class Article
+    {
+        private decimal _price;
+
+        public int ArticleId { get; set; }
+        public string Articlename { get; set; }
+        public decimal Price
+        {
+            get { return this._price;  }
+            set
+            {
+                if (value >= 0.0m)
+                {
+                    this._price = value;
+                }
+            }
+        }
+        public string Description { get; set; }
+
+        public DateTime? ReleaseDate { get; set; }
+
+        // Custroctors
+        public Article() : this(0,"", 0.0m, "", null) { }
+        public Article(int id, string articlename, decimal price, string description, DateTime? releaseDate)
+        {
+            this.ArticleId = id;
+            this.Articlename = articlename;
+            this.Price = price;
+            this.Description = description;
+            this.ReleaseDate = releaseDate;
+        }
+
+        public override string ToString()
+        {
+            return this.ArticleId + " " + this.Articlename + " " + this.Price + "Euro";
+        }
+    }
+}
